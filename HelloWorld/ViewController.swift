6fe4b4d3f14165834e04a3eb33a74e1efaedbbd0
Cephas Maundu <30626432+cmaundu@users.noreply.github.com>
2020-03-24 16:50:09 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Cephas Maundu on 24/03/2020.
+//  Copyright © 2020 Cephas Maundu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    /**
+     Add two numbers together and returns the sum
+     - Parameters:
+        - num1: The first number
+        - num2: The second number
+     */
+    func addNumbers(num1: Int, num2:Int) -> Int{
+        return num1 + num2
+    }
+}
+
